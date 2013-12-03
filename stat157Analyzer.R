bae20e33c6811a6e92ad354113cbd078f3dfefa7
Bonghyun Kim <bkim515@gmail.com>
2013-12-02 17:20:16 -0800

    nov 30

---
+++ stat157Analyzer.R
@@ -0,0 +1,5 @@
+REflection Nov 30
+
+This week has been rather short due to Holidays.
+
+On Tuesday, we had the opportunity to talk to the creator of iPhyton notebook. It was an interesting experience to have one of the creators of a framework that we are using in class to talk directly to us. It really put our work into perspective. The amount of work and effort that probably went into making this framework is unimaginable. I command the creator for his effort in making this open source software for everyone to use.
